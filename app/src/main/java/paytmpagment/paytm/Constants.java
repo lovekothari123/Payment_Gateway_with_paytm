95fb0c488f5904856cdd2f4a2a2d4c26b2f3620c
loveji <love.stegowl123@gmail.com>
2018-06-01 18:07:25 +0530

    Initial commit

---
+++ app/src/main/java/paytmpagment/paytm/Constants.java
@@ -0,0 +1,11 @@
+package paytmpagment.paytm;
+
+public class Constants {
+ 
+    public static final String M_ID = "NextSt66790992489006"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WEP"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+    public static final String WEBSITE = "APP_STAGING";
+    public static final String CALLBACK_URL = "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp";
+ 
+}
